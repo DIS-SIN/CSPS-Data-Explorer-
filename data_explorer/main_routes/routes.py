5e030e8628497d7b7712c2f0fa06bd869c76bbe9
unknown <38829219+deepklim@users.noreply.github.com>
2019-12-02 13:23:21 -0500

    Redirect all routes to /home for users who have routes saved in browsing history

--- data_explorer/main_routes/routes.py
+++ data_explorer/main_routes/routes.py
@@ -1,20 +1,7 @@
 from flask import Blueprint, make_response, render_template, redirect, request, url_for
 from data_explorer import auth
-from data_explorer.config import Config
-from data_explorer.course_routes.queries import browse_queries
 
 main = Blueprint('main', __name__)
-
-
-# Make Registhor API key available to all templates
-GOOGLE_MAPS_API_KEY = Config.GOOGLE_MAPS_API_KEY
-REGISTHOR_API_KEY = Config.REGISTHOR_API_KEY
-@main.context_processor
-def context_processor():
-	return {
-		'GOOGLE_MAPS_API_KEY': GOOGLE_MAPS_API_KEY,
-		'REGISTHOR_API_KEY': REGISTHOR_API_KEY
-	}
 
 
 @main.route('/')
@@ -23,37 +10,21 @@ def splash():
 
 
 @main.route('/about')
-@auth.login_required
-def about():
-	return render_template('about.html')
-
-
-# Browse
 @main.route('/browse')
-@auth.login_required
-def browse():
-	# Only allow 'en' and 'fr' to be passed to app
-	lang = 'fr' if request.cookies.get('lang', None) == 'fr' else 'en'
-	course_list = browse_queries.CourseList(lang).load()
-	pass_dict = course_list._get_nested_dicts()
-	return render_template('browse/browse.html', pass_dict=pass_dict)
-
-
-# Calendar
 @main.route('/calendar')
-@auth.login_required
-def calendar():
-	# Only allow 'en' and 'fr' to be passed to app
-	lang = 'fr' if request.cookies.get('lang', None) == 'fr' else 'en'
-	pass_dict = {}
-	return render_template('calendar/calendar.html', pass_dict=pass_dict)
-
-
-# Coming soon
+@main.route('/course-result')
 @main.route('/departments')
+@main.route('/download-browse')
+@main.route('/download-calendar')
+@main.route('/download-comments')
+@main.route('/download-dashboard')
+@main.route('/download-general')
+@main.route('/download-ratings')
+@main.route('/download-schedule')
+@main.route('/home')
 @auth.login_required
-def departments():
-	return render_template('departments.html')
+def home():
+	return render_template('layout.html')
 
 
 @main.route('/setlang')
@@ -62,7 +33,7 @@ def setlang():
 	"""Allow pages to set cookie 'lang' via query string."""
 	# Redirect pages back to themselves except for splash
 	if request.referrer.endswith('/'):
-		resp = make_response(redirect(url_for('course.home')))
+		resp = make_response(redirect(url_for('main.home')))
 	else:
 		resp = make_response(redirect(request.referrer))
 	# Only allow 'en' and 'fr' to be passed to app
