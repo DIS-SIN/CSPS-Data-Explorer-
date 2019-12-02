47d3a89640f433241902a7885a4727c55aad2d0d
unknown <38829219+deepklim@users.noreply.github.com>
2019-12-02 13:21:08 -0500

    Remove functionality not required for minimal, disclaimer-only version

--- data_explorer/__init__.py
+++ data_explorer/__init__.py
@@ -1,4 +1,3 @@
-import json
 from flask import Flask, render_template, request
 from flask_httpauth import HTTPBasicAuth
 from flask_babel import Babel
@@ -24,29 +23,6 @@ def create_app(config_class=Config):
 	app = Flask(__name__)
 	app.config.from_object(config_class)
 	
-	# Add Python's internal funcs 'any' and 'zip' to Jinja2
-	app.jinja_env.globals.update(any=any) 
-	app.jinja_env.filters['zip'] = zip
-	
-	
-	# Add func to convert Python data structures to JSON
-	def json_filter(my_object):
-		return json.dumps(my_object)
-	app.jinja_env.filters['json_filter'] = json_filter
-	
-	
-	# Add func to get length of nested dictionaries in 'Browse' tab
-	def nested_dict_len(my_dict):
-		nested_len = 0
-		for key, list_ in my_dict.items():
-			nested_len += len(list_)
-		return nested_len
-	app.jinja_env.filters['nested_dict_len'] = nested_dict_len
-	
-	# Register database
-	from data_explorer import db
-	db.init_app(app)
-	
 	
 	# Register plugins
 	@auth.get_password
@@ -68,11 +44,5 @@ def create_app(config_class=Config):
 	
 	# Register blueprints
 	from data_explorer.main_routes.routes import main
-	from data_explorer.course_routes.routes import course
-	from data_explorer.api_routes.routes import api
-	from data_explorer.download_routes.routes import downloads
 	app.register_blueprint(main)
-	app.register_blueprint(course)
-	app.register_blueprint(api)
-	app.register_blueprint(downloads)
 	return app
--- data_explorer/config.py
+++ data_explorer/config.py
@@ -2,16 +2,8 @@ import os
 
 class Config:
 	DEBUG = False
-	LAST_YEAR = '2018-19'
-	THIS_YEAR = '2019-20'
 	BABEL_DEFAULT_LOCALE = 'en'
-	# Options for flask.jsonify
-	JSON_AS_ASCII = False
-	JSONIFY_PRETTYPRINT_REGULAR = True
-	JSON_SORT_KEYS = False
 	# Load strings from environ vars to avoid storing in plaintext
 	BASIC_AUTH_USERNAME = os.environ.get('BASIC_AUTH_USERNAME')
 	BASIC_AUTH_PASSWORD = os.environ.get('BASIC_AUTH_PASSWORD')
 	SECRET_KEY = os.environ.get('SECRET_KEY')
-	GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
-	REGISTHOR_API_KEY = os.environ.get('REGISTHOR_API_KEY')
